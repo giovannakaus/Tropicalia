2bc47f8673c51d08f5eb3f79f3800d5d6ce4c223
giovannakaus <137062159+giovannakaus@users.noreply.github.com>
2025-03-25 11:05:48 -0300

    s

--- script.js
+++ script.js
@@ -29,3 +29,9 @@ botaoDeAcessibilidade.addEventListener('click', function (){
     botaoDeAcessibilidade.classList.toggle('rotacao-botao');
     opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
    })
+
+   const alternaContraste = document.getElementById('alterna-contraste')
+
+   alternaContraste.addEventListener('click', function(){
+    document.body.classList.toggle('alto-contraste')
+})
